15fc323adc2108f0a0ebbefa91abb309cbc6d3a5
Rhyne <rhyneav@gmail.com>
2016-08-21 22:31:16 +0000

    Simplify app, utilize Twitter URL

---
+++ script.js
@@ -0,0 +1,14 @@
+function getword(info,tab) {
+    console.log("Word " + info.selectionText + " was clicked.");
+    var formattedTweet = encodeURIComponent(info.selectionText);
+    
+    chrome.tabs.create({  
+        url: "https://twitter.com/intent/tweet?text=" + formattedTweet,
+    });           
+}
+
+chrome.contextMenus.create({
+    title: "Tweet: %s", 
+    contexts:["selection"], 
+    onclick: getword,
+});
